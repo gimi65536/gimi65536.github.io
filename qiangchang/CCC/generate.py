1cd9c9665ddb9181bfb53d38f42c2f8ff4b60046
gimi65536 <gimi65536@hotmail.com>
2019-03-15 00:16:54 +0800

    107-4

--- qiangchang/CCC/generate.py
+++ qiangchang/CCC/generate.py
@@ -1,21 +1,30 @@
-from typing import List, Tuple
+from typing import List, Tuple, Set
 from datetime import date, timedelta
 
-filename = 'test'
-title = '123'
-event_ID = '456'
-place_order = ['103', '104', '202']
+mask = {0, 1, 2, 3, 4, 5, 6}
 
-one_day = timedelta(days = 1)
-
-def give_newdate(l: List[Tuple[date, date]], weekday: List[int], iso = True):
+def give_newdate(l: List[Tuple[date, date]], weekday: Set[int], iso = True):
 	sol = []
+	if iso:
+		weekday = {i - 1 for i in weekday}
+	weekday = weekday & mask
 	for start, end in l:
-		pass
+		n = (end - start).days
+		start_weekday = start.weekday()
+		pick = [i for i in range(n + 1) if (i + start_weekday) % 7 in weekday]
+		pick_date = [start + timedelta(days = i) for i in pick]
+		sol.extend(pick_date)
+	return sol
 
-if __name__ == '__main__':
+def write_html(filename: str, title: str, event_ID: str, place_order: List[str], l: List[Tuple[date, date]], weekday: Set[int]):
 	with open('template', 'r', newline = '', encoding = 'utf-8') as file:
 		content = file.read()
-	output = content.format(title = title, event_ID = event_ID, place_order = ', '.join(place_order), dates = '')
+	output = content.format(title = title, event_ID = event_ID, place_order = ', '.join(place_order), 
+		dates = ',\n\t\t\t'.join(f'new Date(Date.UTC({d.year}, {d.month - 1}, {d.day}))' for d in give_newdate(l, weekday)))
 	with open(filename, 'w', newline = '\n', encoding = 'utf-8') as file:
 		file.write(output)
+
+if __name__ == '__main__':
+	write_html('107_4/chuangzuo.html', '創作組', '98652', ['103', '104', '202'], [(date(2019, 4, 15), date(2019, 6, 23))], {5})
+	write_html('107_4/qimosheda.html', '期末社大', '98653', ['103', '104', '202'], [(date(2019, 6, 3), date(2019, 6, 14))], {1, 3})
+	write_html('107_4/jiaoxue.html', '教學組', '98655', ['103', '104', '202'], [(date(2019, 4, 22), date(2019, 6, 14))], {2})
